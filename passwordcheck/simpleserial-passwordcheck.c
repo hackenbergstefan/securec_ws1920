d2af4dac975ceab287962e2e915fc525d9ba5b2a
Stefan Hackenberg <mail@stefan-hackenberg.de>
2019-11-28 16:01:00 +0100

    Improve Attack_4_1

--- passwordcheck/simpleserial-passwordcheck.c
+++ passwordcheck/simpleserial-passwordcheck.c
@@ -40,6 +40,44 @@ uint8_t check_password_xor(uint8_t *attempt)
     return passbad;
 }
 
+uint8_t check_password_xor_randomstart(uint8_t *attempt)
+{
+    uint8_t passbad = 0;
+
+    uint8_t j = random_buffer[0] % sizeof(password);
+
+    trigger_high();
+    for (uint8_t i = 0; i < sizeof(password); i++)
+    {
+        passbad |= password[j] ^ attempt[j];
+        j = (j + 1) % sizeof(password);
+    }
+    trigger_low();
+
+    simpleserial_put('r', 1, &passbad);
+
+    return passbad;
+}
+
+uint8_t check_password_xor_randomorder1(uint8_t *attempt)
+{
+    uint8_t passbad = 0;
+
+    uint8_t rand = random_buffer[0] % sizeof(password);
+
+    trigger_high();
+    for (uint8_t i = 0; i < sizeof(password); i++)
+    {
+        uint8_t j = i ^ rand;
+        passbad |= password[j] ^ attempt[j];
+    }
+    trigger_low();
+
+    simpleserial_put('r', 1, &passbad);
+
+    return passbad;
+}
+
 int main(void)
 {
     platform_init();
@@ -50,6 +88,8 @@ int main(void)
     simpleserial_addcmd('r', sizeof(random_buffer), set_random);
     simpleserial_addcmd('p', sizeof(password), set_password);
     simpleserial_addcmd('1', sizeof(password), check_password_xor);
+    simpleserial_addcmd('2', sizeof(password), check_password_xor_randomstart);
+    simpleserial_addcmd('3', sizeof(password), check_password_xor_randomorder1);
     while(1)
         simpleserial_get();
 }
