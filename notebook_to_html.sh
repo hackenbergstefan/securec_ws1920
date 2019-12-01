41de7f930aeb25ffd3e1829b9d83834b6a588a84
Stefan Hackenberg <mail@stefan-hackenberg.de>
2019-12-01 10:40:46 +0100

    Increase timeout

--- notebook_to_html.sh
+++ notebook_to_html.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-NOTEBOOK_PATH="$1" jupyter nbconvert --execute --to html_toc --debug --ExecutePreprocessor.timeout=180 $1
+NOTEBOOK_PATH="$1" jupyter nbconvert --execute --to html_toc --debug --ExecutePreprocessor.timeout=1800 $1
